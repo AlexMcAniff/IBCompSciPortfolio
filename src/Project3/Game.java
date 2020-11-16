59c4425879287f9bb0cfd1f1298d86b8c3cc9c2a
AlexMcAniff <alexmcaniff@gmail.com>
2020-11-16 13:16:39 -0700

    Started TicTacToe Project

---
+++ src/Project3/Game.java
@@ -0,0 +1,28 @@
+package Project3;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class Game extends JPanel {
+    private JButton[] buttons = new JButton[9];
+    public Game(){
+        this.setLayout(new GridLayout(3,3));
+        for (int i = 0; i < 9; i++) {
+            buttons[i] = new JButton();
+            buttons[i].setText("_");
+            this.add(buttons[i]);
+        }
+
+
+    }
+    private class ButtonListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JButton buttonClicked = (JButton) e.getSource();
+            buttonClicked.setText("hello");
+        }
+    }
+}
